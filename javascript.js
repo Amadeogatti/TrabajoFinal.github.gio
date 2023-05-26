679691067383a803253c851985d614f95cf8844b
Amadeogatti <130613996+Amadeogatti@users.noreply.github.com>
2023-05-26 20:40:30 -0300

    25-5-23

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function gracias(porvenir)
+var pregunta = prompt("Es tu primera vez en Gofres?")
+var si = String(si)
+function respuesta(sino)
+alert(pregunta);
+function sino(String){
+if(pregunta == si){ 
+    return "Bienvenid@ por primera vez! Esperamos que disfrutes"
+}
+else{
+    return "Gracias por confiar otra vez en nosotros!"
+ }
+
+}
+ alert(sino);
